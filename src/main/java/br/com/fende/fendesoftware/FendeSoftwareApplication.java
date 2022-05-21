429b89779ba980e36de4be6928c481a304c36c2b
Kristten <kristten258@gmail.com>
2022-05-21 15:13:19 -0300

    First commit

---
+++ src/main/java/br/com/fende/fendesoftware/FendeSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fende.fendesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FendeSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FendeSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fende/fendesoftware/FendeSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fende.fendesoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FendeSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
